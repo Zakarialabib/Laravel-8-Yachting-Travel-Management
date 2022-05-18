a325942616e801e65dc753b7d80a83ed2d9950a9
Zakarialabib <58374576+Zakarialabib@users.noreply.github.com>
2022-05-18 21:05:47 +0100

    Update home.blade.php

--- resources/views/pages/frontend/home.blade.php
+++ resources/views/pages/frontend/home.blade.php
@@ -17,7 +17,7 @@
                             <div class="b-main-slider__title-wrap">
                                 <div class="b-main-slider__slogan">{{$slider->slogan}}</div>
                                 <div class="b-main-slider__title">{{$slider->title}}</div>
-                                <div class="b-main-slider__label text-secondary">{{$slider->label}}</div>
+                                <div class="b-main-slider__label text-secondary"><a href="{{$slider->link}}">{{$slider->label}}</a></div>
                             </div>
                         </div>
                     </div>
@@ -31,131 +31,6 @@
 <div class="b-about">
     <div class="ui-decor ui-decor_down"></div>
     <div class="container">
-        {{-- <div class="b-main-filter-content tab-content" id="findTabContent">
-            <div class="tab-pane fade show active" id="content-allCar">
-                <div class="row align-items-end no-gutters">
-                    <div class="b-main-filter__main col-lg">
-                          <div class="b-main-filter__inner row no-gutters">
-                            <div class="b-main-filter__item col-md-4">
-                                <div class="b-main-filter__label">Select Boat Type</div>
-                                <div class="b-main-filter__selector">
-                                    <select class="selectpicker" data-width="100%" data-style="ui-select">
-                                        <option>Celebrations</option>
-                                        <option>Fishing</option>
-                                        <option>Journey</option>
-                                    </select>
-                                </div>
-                            </div>
-                            <div class="b-main-filter__item col-md-4">
-                                <div class="b-main-filter__label">Select a Model</div>
-                                <div class="b-main-filter__selector">
-                                    <select class="selectpicker" data-width="100%" data-style="ui-select">
-                                        <option>Model 1</option>
-                                        <option>Model 2</option>
-                                        <option>Model 3</option>
-                                    </select>
-                                </div>
-                            </div>
-                            <div class="b-main-filter__item col-md-4">
-                                <div class="b-main-filter__label">Price Range</div>
-                                <div class="b-main-filter__selector">
-                                    <select class="selectpicker" data-width="100%" data-style="ui-select">
-                                        <option>Max $50 per day</option>
-                                        <option>Max $100 per day</option>
-                                        <option>Max $150 per day</option>
-                                    </select>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-auto">
-                        <button class="b-main-filter__btn btn btn-secondary">Search</button>
-                    </div>
-                </div>
-            </div>
-            <div class="tab-pane fade" id="content-newCars">
-                <div class="row align-items-end no-gutters">
-                    <div class="b-main-filter__main col-lg">
-                        <div class="b-main-filter__inner row no-gutters">
-                            <div class="b-main-filter__item col-md-4">
-                                <div class="b-main-filter__label">Select Make</div>
-                                <div class="b-main-filter__selector">
-                                    <select class="selectpicker" data-width="100%" data-style="ui-select">
-                                        <option>Audi</option>
-                                        <option>BMV</option>
-                                        <option>Opel</option>
-                                    </select>
-                                </div>
-                            </div>
-                            <div class="b-main-filter__item col-md-4">
-                                <div class="b-main-filter__label">Select a Model</div>
-                                <div class="b-main-filter__selector">
-                                    <select class="selectpicker" data-width="100%" data-style="ui-select">
-                                        <option>Model 1</option>
-                                        <option>Model 2</option>
-                                        <option>Model 3</option>
-                                    </select>
-                                </div>
-                            </div>
-                            <div class="b-main-filter__item col-md-4">
-                                <div class="b-main-filter__label">Price Range</div>
-                                <div class="b-main-filter__selector">
-                                    <select class="selectpicker" data-width="100%" data-style="ui-select">
-                                        <option>Max $5000</option>
-                                        <option>Max $15000</option>
-                                        <option>Max $25000</option>
-                                    </select>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-auto">
-                        <button class="b-main-filter__btn btn btn-secondary">Search</button>
-                    </div>
-                </div>
-            </div>
-            <div class="tab-pane fade" id="content-usedCars">
-                <div class="row align-items-end no-gutters">
-                    <div class="b-main-filter__main col-lg">
-                        <div class="b-main-filter__inner row no-gutters">
-                            <div class="b-main-filter__item col-md-4">
-                                <div class="b-main-filter__label">Select Make</div>
-                                <div class="b-main-filter__selector">
-                                    <select class="selectpicker" data-width="100%" data-style="ui-select">
-                                        <option>Audi</option>
-                                        <option>BMV</option>
-                                        <option>Opel</option>
-                                    </select>
-                                </div>
-                            </div>
-                            <div class="b-main-filter__item col-md-4">
-                                <div class="b-main-filter__label">Select a Model</div>
-                                <div class="b-main-filter__selector">
-                                    <select class="selectpicker" data-width="100%" data-style="ui-select">
-                                        <option>Model 1</option>
-                                        <option>Model 2</option>
-                                        <option>Model 3</option>
-                                    </select>
-                                </div>
-                            </div>
-                            <div class="b-main-filter__item col-md-4">
-                                <div class="b-main-filter__label">Price Range</div>
-                                <div class="b-main-filter__selector">
-                                    <select class="selectpicker" data-width="100%" data-style="ui-select">
-                                        <option>Max $5000</option>
-                                        <option>Max $15000</option>
-                                        <option>Max $25000</option>
-                                    </select>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-auto">
-                        <button class="b-main-filter__btn btn btn-secondary">Search</button>
-                    </div>
-                </div>
-            </div>
-        </div> --}}
         <div class="row">
             <div class="col-lg-6 ">
                 <div class="text-left">
@@ -280,9 +155,9 @@
         <div class="row">
             <div class="col-12 col-md-10 col-lg-10">
                 <div class="video-info">
-                    <p><img src="assets/img/decore02.png" alt="decore">Give us a call or drop an email, We endeavor to answer within 24 hours</p>
-                    <h4>We’ve Exclusive Boats With Charter Offers</h4>
-                    <h5>LET’S PLAN YOUR NEXT TOUR!</h5>
+                    <p><img src="assets/img/decore02.png" alt="decore">Plan your cruise around Santorini</p>
+                    <h4>Luxury Yacht Tours to experience</h4>
+                    <h5>Contact us to plan your next tour!</h5>
                     <ul>
                         <li><i class="fas fa-phone-square"></i> Call Us Today: {{setting('home_phone')}}</li>
                         <li><i class="fas fa-envelope-square"></i> Email: <a href="mailto:{{setting('home_email')}}">{{setting('home_email  ')}}</a></li>
@@ -372,7 +247,7 @@
                     <div class="card">
                     <div class="card-header" id="heading1">
                         <h3 class="mb-0">
-                        <button class="ui-accordion__link collapsed" type="button" data-toggle="collapse" data-target="#collapse1" aria-expanded="true" aria-controls="collapse1"><span class="ui-accordion__number">*</span>{{$faq->title}}<i class="ic fas fa-chevron-down"></i></button>
+                        <button class="ui-accordion__link collapsed" type="button" data-toggle="collapse" data-target="#collapse1" aria-expanded="true" aria-controls="collapse1"><span class="ui-accordion__number">{{$faq->id}}</span>{{$faq->title}}<i class="ic fas fa-chevron-down"></i></button>
                         </h3>
                     </div>
                     <div class="collapse show" id="collapse1" data-aria-labelledby="heading1" data-parent="#accordion-{{$faq->id}}">
